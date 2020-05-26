0c7cc8a6efc1a86034b3859d247aca8059418051
USAMI Kosuke <usami.kosuke@gmail.com>
2020-05-27 08:58:03 +0900

    Chapter 2 : Getting Started with F#

---
+++ ExpertFSharpScripts/02GettingStarted.fsx
@@ -0,0 +1,15 @@
+﻿let splitAtSpaces (text: string) =
+    text.Split ' '
+    |> Array.toList
+
+let wordCount text =
+    let words = splitAtSpaces text
+    let numWords = words.Length
+    let distinctWords = List.distinct words
+    let numDups = numWords - distinctWords.Length
+    (numWords, numDups)
+
+let showWordCount text =
+    let numWords, numDups = wordCount text
+    printfn "--> %d words in the text" numWords
+    printfn "--> %d duplicate words" numDups
