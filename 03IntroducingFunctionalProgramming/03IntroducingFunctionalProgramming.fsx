19dbe12016c18c85440d38433b7403cecd5b7376
USAMI Kosuke <usami.kosuke@gmail.com>
2020-06-03 23:32:25 +0900

    Defining Recursive Functions

---
+++ 03IntroducingFunctionalProgramming/03IntroducingFunctionalProgramming.fsx
@@ -0,0 +1,10 @@
+﻿// Defining Recursive Functions
+
+let rec factorial num =
+    if num <= 1 then 1
+    else num * factorial (num - 1)
+
+let rec length xs =
+    match xs with
+    | [] -> 0
+    | _ :: tail -> 1 + length tail
