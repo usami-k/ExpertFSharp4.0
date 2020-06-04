4b2d1b72752dadda6b91640fc387b9661d8f9641
USAMI Kosuke <usami.kosuke@gmail.com>
2020-06-04 09:00:53 +0900

    Mutually recursive functions

--- 03IntroducingFunctionalProgramming/03IntroducingFunctionalProgramming.fsx
+++ 03IntroducingFunctionalProgramming/03IntroducingFunctionalProgramming.fsx
@@ -8,3 +8,16 @@ let rec length xs =
     match xs with
     | [] -> 0
     | _ :: tail -> 1 + length tail
+
+// Mutually recursive functions
+let rec even num = (num = 0u) || odd (num - 1u)
+
+and odd num = (num <> 0u) && even (num - 1u)
+
+let rec even2 num =
+    if num = 0u then true
+    else odd2 (num - 1u)
+
+and odd2 num =
+    if num <> 0u then even2 (num - 1u)
+    else false
