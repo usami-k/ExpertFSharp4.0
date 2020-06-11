1e1bb87322dd1d08ebcb044e420c4ecba4736ec1
USAMI Kosuke <usami.kosuke@gmail.com>
2020-06-12 08:15:12 +0900

    Using Mutable Records

--- 04IntroducingImperativeProgramming/Script.fsx
+++ 04IntroducingImperativeProgramming/Script.fsx
@@ -26,3 +26,28 @@ open System.Text.RegularExpressions
 
 for m in Regex.Matches("All the Pretty Horses", "[a-zA-Z]+") do
     printfn "res = %s" m.Value
+
+// Using Mutable Records
+
+type DiscreteEventCounter =
+    { mutable Total : int
+      mutable Positive : int
+      Name : string }
+
+let newCounter nm =
+    { Total = 0
+      Positive = 0
+      Name = nm }
+
+let longPageCounter = newCounter "long page(s)"
+
+let reportStatus (s : DiscreteEventCounter) = printfn "We have %d %s out of %d" s.Positive s.Name s.Total
+
+let recordEvent (s : DiscreteEventCounter) isPositive =
+    s.Total <- s.Total + 1
+    if isPositive then s.Positive <- s.Positive + 1
+
+let fetch url =
+    let page = http url
+    recordEvent longPageCounter (page.Length > 10000)
+    page
