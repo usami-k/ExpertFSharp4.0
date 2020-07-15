05f25abb931495137f1e9b12099504c3673294fe
USAMI Kosuke <usami.kosuke@gmail.com>
2020-07-15 22:03:43 +0900

    Start Chapter 05

---
+++ 05UnderstandingTypes/Script.fsx
@@ -0,0 +1,22 @@
+﻿// Defining Discriminated Unions
+
+type Route = int
+
+type Make = string
+
+type Model = string
+
+type Transport =
+    | Car of Make * Model
+    | Bicycle
+    | Bus of Route
+
+let ian = Car("BMW", "360")
+
+let don =
+    [ Bicycle
+      Bus 8 ]
+
+let peter =
+    [ Car("Ford", "Fiesta")
+      Bicycle ]
